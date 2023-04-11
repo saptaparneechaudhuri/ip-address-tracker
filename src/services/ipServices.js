6d48a716e77e44116d5eff31a8e94b1774f40fbc
Saptaparnee Chaudhuri <saptaparnee8@gmail.com>
2023-04-11 19:22:55 +0530

    ip address tracker

--- src/services/ipServices.js
+++ src/services/ipServices.js
@@ -6,11 +6,23 @@ export const ipAddressAPI = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: `https://geo.ipify.org/api/v2` }),
   endpoints: (builder) => ({
     trackIP: builder.query({
-      query: (inputTerm) => ({
-        url: `/country,city?apiKey=${process.env.REACT_APP_API_KEY}`,
-        params: { ipAddress: inputTerm },
-        method: "GET",
-      }),
+      query: (inputTerm) => {
+        const checkIP =
+          /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
+
+        const checkDomain =
+          /^[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/gm;
+
+        return {
+          url: `/country,city?apiKey=${process.env.REACT_APP_API_KEY}`,
+          params: checkIP.test(inputTerm)
+            ? { ipAddress: inputTerm }
+            : checkDomain.test(inputTerm)
+            ? { domain: inputTerm }
+            : "",
+          method: "GET",
+        };
+      },
     }),
   }),
 });
